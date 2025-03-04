3f5ff27998b30230bdf458f72911247970171726
Dev Mandal <devmandal2004@gmail.com>
2025-03-04 13:10:40 +0530

    removing Reverse proxy test

--- app/providers.tsx
+++ app/providers.tsx
@@ -52,8 +52,8 @@ export function Providers({ children }: { children: React.ReactNode }) {
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
 	useEffect(() => {
 		posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-			api_host: "/ingest",
-			ui_host: "https://us.posthog.com",
+			api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+			// ui_host: "https://us.posthog.com",
 			capture_pageview: false, // Disable automatic pageview capture, as we capture manually
 			person_profiles: "always", // or 'always' to create profiles for anonymous users as well
 		});
